a92af178b4e4aa9e6825ec314e32f9c4a6f17e5f
Maxim Shurygin <maximys2142@mail.ru>
2023-06-17 16:30:15 +0700

    added more functions

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,5 @@
 from .utils import youtube, printj
+import json
 
 
 class Channel:
@@ -6,8 +7,39 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.channel_info = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
+        snippet = self.channel_info['items'][0]['snippet']
+        statistics = self.channel_info['items'][0]['statistics']
+
+        self.title = snippet['title']
+        self.description = snippet['description']
+        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.subscriber_count = int(statistics['subscriberCount'])
+        self.video_count = int(statistics['videoCount'])
+        self.view_count = int(statistics['viewCount'])
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        return youtube
+
+    def to_json(self, filename):
+        channel_data = {
+            "channel_id": self.channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscriber_count": self.subscriber_count,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(channel_data, file)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
