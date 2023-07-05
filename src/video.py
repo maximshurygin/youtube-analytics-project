ac675dd1b31248f0973b5650b0ec06f720486169
Maxim Shurygin <maximys2142@mail.ru>
2023-07-05 12:15:12 +0400

    added try/except to class Video

--- src/video.py
+++ src/video.py
@@ -4,18 +4,23 @@ from .utils import youtube
 class Video:
     def __init__(self, video_id):
         self.video_id = video_id
-
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.video_url = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            self.title = video_response['items'][0]['snippet']['title']
+            self.video_url = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except Exception:
+            self.title = None
+            self.video_url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         """возвращает информацию для пользователя"""
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
